6158061aee5630e2464f1a4f623f4522e1a476a5
rokumura7 <rokumura.8807@gmail.com>
2020-08-09 09:46:32 +0900

    implement crawler-mock

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-(()=>{console.log('Hello World.')})()
+import puppeteer from 'puppeteer'
+import { Page } from 'puppeteer'
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://www.yurindo.co.jp/ranking/week-all').then(() => page.waitFor(1000))
+  const list = await page.$$('.rank_list')
+  const book = []
+  for (let i = 1; i <= list.length; i++) {
+    const title = await page.$eval(`.rank_list:nth-child(${i})  .list_title > span`, elm => elm.textContent)
+    console.log(title)
+  }
+  await browser.close()
+})()
