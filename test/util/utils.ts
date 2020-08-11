a30cfef76afe63d110cf2de4ce7ed0a33b97af3f
rokumura7 <rokumura.8807@gmail.com>
2020-08-11 20:57:35 +0900

    added test

---
+++ test/util/utils.ts
@@ -0,0 +1,7 @@
+import { describe, it } from 'mocha'
+import { assert } from 'chai'
+import * as utils from '../../src/util/utils'
+
+describe('Test for utils.ts', () => {
+  it('replaceIndex', () => assert.equal(utils.replaceIndex('td[_INDEX]', 1), 'td[1]'))
+})
