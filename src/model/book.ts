4244b887149219d0d3beeb8cef96a9544518bb5a
rokumura7 <rokumura.8807@gmail.com>
2020-08-12 01:18:56 +0900

    renamed value objects

--- src/model/book.ts
+++ src/model/book.ts
@@ -1,9 +1,15 @@
+import { Title, of as _title } from "./vo/book/title"
+import { Author, of as _author } from "./vo/book/author"
+import { Price, of as _price } from "./vo/book/price"
+import { Publisher, of as _publisher } from "./vo/book/publisher"
+import { ISBN, of as _isbn } from "./vo/book/isbn"
+
 export default class Book {
-  title: string | null
-  author: string | null
-  price: string | null
-  publisher: string | null
-  isbn: string | null
+  title: Title | null
+  author: Author | null
+  price: Price | null
+  publisher: Publisher | null
+  isbn: ISBN | null
 
   constructor(
     title: string | null,
@@ -12,10 +18,10 @@ export default class Book {
     publisher: string | null,
     isbn: string | null
   ) {
-    this.title = title
-    this.author = author
-    this.price = price
-    this.publisher = publisher
-    this.isbn = isbn
+    this.title = _title(title)
+    this.author = _author(author)
+    this.price = _price(price)
+    this.publisher = _publisher(publisher)
+    this.isbn = _isbn(isbn)
   }
 }
