fca6ec6103ad59733d73e048fb36c0d5eaef0891
rokumura7 <rokumura.8807@gmail.com>
2020-08-11 20:42:05 +0900

    added price, publisher and isbn

--- src/crawlers/1/yurindo_crawler.ts
+++ src/crawlers/1/yurindo_crawler.ts
@@ -12,7 +12,10 @@ import Book from '../../model/book'
   for (let i = 1; i <= list.length; i++) {
     const title = await page.$eval(utils.replaceIndex(SELECTORS.TITLE, i), elm => elm.textContent)
     const author = await page.$eval(utils.replaceIndex(SELECTORS.AUTHOR, i), elm => elm.textContent)
-    books.push(new Book(title, author))
+    const price = await page.$eval(utils.replaceIndex(SELECTORS.PRICE, i), elm => elm.textContent)
+    const publisher = await page.$eval(utils.replaceIndex(SELECTORS.PUBLISHER, i), elm => elm.textContent)
+    const isbn = await page.$eval(utils.replaceIndex(SELECTORS.ISBN, i), elm => elm.textContent)
+    books.push(new Book(title, author, price, publisher, isbn))
   }
   console.log(books)
   await browser.close()
--- src/model/book.ts
+++ src/model/book.ts
@@ -1,8 +1,21 @@
 export default class Book {
   title: string | null
   author: string | null
-  constructor(title: string | null, author: string | null) {
+  price: string | null
+  publisher: string | null
+  isbn: string | null
+
+  constructor(
+    title: string | null,
+    author: string | null,
+    price: string | null,
+    publisher: string | null,
+    isbn: string | null
+  ) {
     this.title = title
     this.author = author
+    this.price = price
+    this.publisher = publisher
+    this.isbn = isbn
   }
 }
