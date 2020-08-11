657234c880acf0fd7cd0742e6ba6a206237784e5
rokumura7 <rokumura.8807@gmail.com>
2020-08-12 02:16:20 +0900

    replaced constructor to builder

--- src/crawlers/1/yurindo_crawler.ts
+++ src/crawlers/1/yurindo_crawler.ts
@@ -1,21 +1,36 @@
 import puppeteer from 'puppeteer'
 import SELECTORS from './selectors'
 import * as utils from '../../util/utils'
-import Book from '../../model/book'
+import { Book } from '../../model/book'
+import { of as _title } from '../../model/vo/book/title'
+import { of as _author } from '../../model/vo/book/author'
+import { of as _price } from '../../model/vo/book/price'
+import { of as _publisher } from '../../model/vo/book/publisher'
+import { of as _isbn } from '../../model/vo/book/isbn'
+import BookBuilder from '../../model/bookBuilder'
 
 (async () => {
+  console.log('start')
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   await page.goto('https://www.yurindo.co.jp/ranking/week-all').then(() => page.waitFor(1000))
   const list = await page.$$(SELECTORS.RANK_LIST)
   const books: Book[] = []
   for (let i = 1; i <= list.length; i++) {
-    const title = await page.$eval(utils.replaceIndex(SELECTORS.TITLE, i), elm => elm.textContent)
-    const author = await page.$eval(utils.replaceIndex(SELECTORS.AUTHOR, i), elm => elm.textContent)
-    const price = await page.$eval(utils.replaceIndex(SELECTORS.PRICE, i), elm => elm.textContent)
-    const publisher = await page.$eval(utils.replaceIndex(SELECTORS.PUBLISHER, i), elm => elm.textContent)
-    const isbn = await page.$eval(utils.replaceIndex(SELECTORS.ISBN, i), elm => elm.textContent)
-    books.push(new Book(title, author, price, publisher, isbn))
+    const title = _title(await page.$eval(utils.replaceIndex(SELECTORS.TITLE, i), elm => elm.textContent))
+    const author = _author(await page.$eval(utils.replaceIndex(SELECTORS.AUTHOR, i), elm => elm.textContent))
+    const price = _price(await page.$eval(utils.replaceIndex(SELECTORS.PRICE, i), elm => elm.textContent))
+    const publisher = _publisher(await page.$eval(utils.replaceIndex(SELECTORS.PUBLISHER, i), elm => elm.textContent))
+    const isbn = _isbn(await page.$eval(utils.replaceIndex(SELECTORS.ISBN, i), elm => elm.textContent))
+    // TODO: Book's properties are not private. They can be written directly even after build().
+    const book = new BookBuilder()
+      ._title(title)
+      ._author(author)
+      ._price(price)
+      ._publisher(publisher)
+      ._isbn(isbn)
+      .build()
+    books.push(book)
   }
   console.log(books)
   await browser.close()
--- src/model/book.ts
+++ src/model/book.ts
@@ -1,27 +1,21 @@
-import { Title, of as _title } from "./vo/book/title"
-import { Author, of as _author } from "./vo/book/author"
-import { Price, of as _price } from "./vo/book/price"
-import { Publisher, of as _publisher } from "./vo/book/publisher"
-import { ISBN, of as _isbn } from "./vo/book/isbn"
+import { Title } from "./vo/book/title"
+import { Author } from "./vo/book/author"
+import { Price } from "./vo/book/price"
+import { Publisher } from "./vo/book/publisher"
+import { ISBN } from "./vo/book/isbn"
 
-export default class Book {
-  title: Title | null
-  author: Author | null
-  price: Price | null
-  publisher: Publisher | null
-  isbn: ISBN | null
+interface Book {
+  title: Title
+  author: Author
+  price: Price
+  publisher: Publisher
+  isbn: ISBN
+}
 
-  constructor(
-    title: string | null,
-    author: string | null,
-    price: string | null,
-    publisher: string | null,
-    isbn: string | null
-  ) {
-    this.title = _title(title)
-    this.author = _author(author)
-    this.price = _price(price)
-    this.publisher = _publisher(publisher)
-    this.isbn = _isbn(isbn)
+class Book implements Book {
+  constructor(book: Book) {
+    Object.assign(this, book)
   }
 }
+
+export { Book }
