99cdb3edf41bdedd8092083d3fb726635ab59863
rokumura7 <rokumura.8807@gmail.com>
2020-08-14 14:36:50 +0900

    split a function from main code

--- src/crawlers/1/yurindo_crawler.ts
+++ src/crawlers/1/yurindo_crawler.ts
@@ -9,6 +9,10 @@ import { Publisher } from '../../model/vo/book/publisher'
 import { ISBN } from '../../model/vo/book/isbn'
 import of from '../../model/vo/generator'
 
+const text = async (page: Page, selector: string, i: number): Promise<string | null> => {
+  return await page.$eval(utils.replaceIndex(selector, i), elm => elm.textContent)
+}
+
 (async () => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
@@ -16,11 +20,11 @@ import of from '../../model/vo/generator'
   const list = await page.$$(SELECTORS.RANK_LIST)
   const books: Book[] = []
   for (let i = 1; i <= list.length; i++) {
-    const title = await page.$eval(utils.replaceIndex(SELECTORS.TITLE, i), elm => elm.textContent)
-    const author = await page.$eval(utils.replaceIndex(SELECTORS.AUTHOR, i), elm => elm.textContent)
-    const price = await page.$eval(utils.replaceIndex(SELECTORS.PRICE, i), elm => elm.textContent)
-    const publisher = await page.$eval(utils.replaceIndex(SELECTORS.PUBLISHER, i), elm => elm.textContent)
-    const isbn = await page.$eval(utils.replaceIndex(SELECTORS.ISBN, i), elm => elm.textContent)
+    const title = await text(page, SELECTORS.TITLE, i)
+    const author = await text(page, SELECTORS.AUTHOR, i)
+    const price = await text(page, SELECTORS.PRICE, i)
+    const publisher = await text(page, SELECTORS.PUBLISHER, i)
+    const isbn = await text(page, SELECTORS.ISBN, i)
     const book: Book = {
       title: of<Title>(title),
       author: of<Author>(author),
