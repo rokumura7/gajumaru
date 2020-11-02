e04aed317aae95da4bf51290a2b4b8aa3279eea7
rokumura7 <rokumura.8807@gmail.com>
2020-11-02 23:11:28 +0900

    add wait method

--- src/lib/util/Utils.ts
+++ src/lib/util/Utils.ts
@@ -1,2 +1,5 @@
 export const replaceIndex = (selector: string, index: number): string =>
   selector.replace(/_INDEX/, index + '');
+
+export const wait = async (sec = 1): Promise<void> =>
+  new Promise((resolve) => setTimeout(resolve, sec * 1000));
