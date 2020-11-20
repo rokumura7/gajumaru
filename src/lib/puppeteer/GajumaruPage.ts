9682a52f6f217d5f3bc4c40344a066f1f3d0973c
rokumura7 <rokumura.8807@gmail.com>
2020-11-20 21:46:11 +0900

    make GajumaruPage functionally

--- src/lib/puppeteer/GajumaruPage.ts
+++ src/lib/puppeteer/GajumaruPage.ts
@@ -10,15 +10,16 @@ class GajumaruPage {
 
   static build = (_page: Page): GajumaruPage => new GajumaruPage(_page);
 
-  async elm<T>(selector: string): Promise<T> {
-    return of(await this.page.$eval(selector, (elm) => elm.textContent));
-  }
+  elm = async <T>(selector: string): Promise<T> =>
+    await this.page
+      .$eval(selector, (elm) => elm.textContent)
+      .then((v) => of(v));
 
-  async goto(url: string): Promise<Response | null> {
+  goto = async (url: string): Promise<Response | null> => {
     const res = await this.page.goto(url);
     await wait();
     return res;
-  }
+  };
 
   $$ = async (selector: string): Promise<ElementHandle<Element>[]> =>
     await this.page.$$(selector);
