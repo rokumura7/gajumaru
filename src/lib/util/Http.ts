f9889b3a8b501adad2844dbdab0b73ac1e2ca9bd
rokumura7 <rokumura.8807@gmail.com>
2020-11-23 20:23:30 +0900

    add http client

---
+++ src/lib/util/Http.ts
@@ -0,0 +1,10 @@
+import axios, { AxiosResponse } from 'axios';
+
+const client = axios.create({
+  baseURL: 'http://0.0.0.0:8080/api/v1',
+});
+
+export const req = async (
+  uri: string,
+  json: { [key: string]: string }
+): Promise<AxiosResponse> => await client.post(uri, json);
