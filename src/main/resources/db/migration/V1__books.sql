fd984bf2998ef3ff8b076faf943c5ad0ee08a714
rokumura7 <rokumura.8807@gmail.com>
2020-11-29 16:44:56 +0900

    remove flyway migration

--- src/main/resources/db/migration/V1__books.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `books` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(255) NOT NULL,
-    `price` INT(11),
-    `isbn` INT(11),
-    `publisher_id` INT(11),
-    PRIMARY KEY (`id`)
-);
