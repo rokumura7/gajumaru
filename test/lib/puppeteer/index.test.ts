cbbe2a09e49b7d2b846a4d60b61f88450777f3b3
rokumura7 <rokumura.8807@gmail.com>
2021-01-05 22:28:03 +0900

    added ut for puppeteer/index.ts

---
+++ test/lib/puppeteer/index.test.ts
@@ -0,0 +1,11 @@
+import { GajumaruBrowser, GajumaruPage } from '@/lib/puppeteer';
+
+describe('puppeteer', () => {
+  test('GajumaruPuppeteer', async () => {
+    const browser = await GajumaruBrowser.build();
+    expect(browser).toBeInstanceOf(GajumaruBrowser);
+
+    const page = await browser.newPage();
+    expect(page).toBeInstanceOf(GajumaruPage);
+  });
+});
